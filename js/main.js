00b291d1cb7fb6490166fd4c8eecd8d5a3128ea2
Jordan <chase@chaseu.com>
2014-10-24 18:35:29 -0400

    First commit of homepage template.  Still needs JS functionality and CSS cleanup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+(function($){
 
+ //globals
+ var interval = 5000;
+
+ //functions
+ function runGallery(){
+    Galleria.configure({
+      imageCrop:true,
+      height:$(".gallery").width()/1.3,
+      responsive:true,
+      autoplay:interval
+    })  
+    Galleria.run('.gallery');
+  }
+  
+  window.onload = function(){
+    runGallery();
+    $(".carousel").carousel({interval:interval});
+  }
+  
+  $(window).bind('resize', function(e)
+  {
+      window.resizeEvt;
+      $(window).resize(function()
+      {
+          clearTimeout(window.resizeEvt);
+          window.resizeEvt = setTimeout(function()
+          {
+              //code to do after window is resized
+              runGallery();
+          }, 250);
+      });
+  });
+  
+})(jQuery);
