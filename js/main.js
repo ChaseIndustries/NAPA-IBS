88f2fa5d440c2aa0a9984d054a7061f62ea3437e
Jordan <chase@chaseu.com>
2014-10-28 18:09:54 -0400

    Added some megamenus

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 (function($){
 
  //globals
- var interval = 5000;
+ var interval = 5000, 
+ timers = {} ;
 
  //functions
  function runGallery(){
@@ -20,6 +21,7 @@
     $(".carousel").carousel({interval:interval});
   }
   
+  //listeners
   $(window).bind('resize', function(e)
   {
       window.resizeEvt;
@@ -33,5 +35,56 @@
           }, 250);
       });
   });
+  
+  var megaMenus = {
+    close: function(){
+    
+      $(".mm.open").removeClass("open");
+      $(".mm.active").removeClass("active");
+      
+    },
+    open: function(el){
+    
+      clearTimeout(timers.mm);
+      if($(el).hasClass("active")){      
+        
+        $(el).removeClass("active");
+      
+      } else {
+        
+        $(".mm.active").removeClass("active");
+        var classes = "active";
+        if(!$(".mm.open").length){
+          $(".mm").addClass("open");
+        }
+        $(el).addClass(classes);
+      
+      }
+    }
+    
+  }
+  $("[data-toggle='mm']").hover(function(){
+      
+      var el = $(this).attr("data-target");
+      if(!el){
+        throw("Missing data-target attribute");
+        return false;
+      }
+      $(this).addClass("active"); 
+      megaMenus.open(el);
+      
+  }, function(){
+    timers.mm = setTimeout(function(){
+      megaMenus.close();
+    },1000);
+  });
+  $(".mm").mouseover(function(){
+    clearTimeout(timers.mm);
+  })
+  $(".mm").mouseleave(function(){
+      timers.mm = setTimeout(function(){
+        megaMenus.close();
+      },1000);
+  })
   
 })(jQuery);
