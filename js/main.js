ea5d6ebf9f349c8a88d6409304e8babf99dfb81a
Jordan <chase@chaseu.com>
2014-10-30 00:19:36 -0400

    Added more JS functionality, added state.css, more components, renamed some classes, etc.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 (function($){
 
- //globals
- var interval = 5000, 
- timers = {} ;
+  //globals
+  
+  var interval = 5000, 
+  timers = {},
+  megaMenus = {
+    close: function(){
+      $(".mm.open").removeClass("open");
+      $(".mm.active, .top-button.active").removeClass("active");
+    },
+    open: function(el){ 
+      clearTimeout(timers.mm);
+      if(!$(el).hasClass("active")){      
+        $(".mm.active").removeClass("active");
+        var classes = "active";
+        if(!$(".mm.open").length){
+          $(".mm").addClass("open");
+        }
+        $(el).addClass(classes);
+      }
+    },
+    init: function(){
+      var maxHeight = $(".mm:first").outerHeight();
+      $(".mm").css("height",maxHeight);
+      $(".mm").each(function(){
+        var h = $(this).outerHeight();
+        if(h > maxHeight){
+          maxHeight = h;
+          $(".mm").css("height",maxHeight);
+        }
+      })
+    }
+  }
+  
+  //functions
 
- //functions
- function runGallery(){
+  function runGallery(){
     /**
-     * other galleria configurations located in /js/galleria.ibs.js 
+     * other galleria configurations located in /js/galleria.ibs.js, styles in /css/galleria.ibs.css
      */
     Galleria.configure({
       height:$(".gallery").width()/1.3,
@@ -16,12 +46,17 @@
     Galleria.run('.gallery');
   }
   
+  //page load
+  
   window.onload = function(){
     runGallery();
+    megaMenus.init();
     $(".carousel").carousel({interval:interval});
   }
   
   //listeners
+  
+  //stuff to do on resize
   $(window).bind('resize', function(e)
   {
       window.resizeEvt;
@@ -36,42 +71,32 @@
       });
   });
   
-  var megaMenus = {
-    close: function(){
-    
-      $(".mm.open").removeClass("open");
-      $(".mm.active").removeClass("active");
-      
-    },
-    open: function(el){
-    
-      clearTimeout(timers.mm);
-      if($(el).hasClass("active")){      
-        
-        $(el).removeClass("active");
-      
-      } else {
-        
-        $(".mm.active").removeClass("active");
-        var classes = "active";
-        if(!$(".mm.open").length){
-          $(".mm").addClass("open");
-        }
-        $(el).addClass(classes);
-      
-      }
-    }
-    
-  }
+/*
+  $("carousel__lg").on("slid.bs.carousel",function(e){
+    item = e.dataTarget;
+    item.find(".carousel-icon").addClass("active");
+  });
+  
+  $("carousel__lg").on("slide.bs.carousel",function(e){
+    item = e.dataTarget;
+    item.find(".carousel-icon").removeClass("active");
+  });
+*/
+  
   $("[data-toggle='mm']").hover(function(){
-      
-      var el = $(this).attr("data-target");
-      if(!el){
-        throw("Missing data-target attribute");
-        return false;
-      }
-      $(this).addClass("active"); 
-      megaMenus.open(el);
+      var me = $(this);
+      //set a timeout to reduce accidentally rolling over the item
+      clearTimeout(timers.mmHover);
+      timers.mmHover = setTimeout(function(){
+          var el = me.attr("data-target");
+          if(!el){
+            throw("Missing data-target attribute");
+            return false;
+          }
+          $(".top-button.active").removeClass("active");
+          me.addClass("active"); 
+          megaMenus.open(el);
+      },.1);
       
   }, function(){
     timers.mm = setTimeout(function(){
@@ -80,6 +105,7 @@
   });
   $(".mm").mouseover(function(){
     clearTimeout(timers.mm);
+    clearTimeout(timers.mmHover);
   })
   $(".mm").mouseleave(function(){
       timers.mm = setTimeout(function(){
